c308e074ad88b4de313073d1facbf2eedf8e898c
Phatjiro <nguyenvanphat911@gmail.com>
2022-11-27 10:35:33 +0700

    add missing file

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainContainers from './navigation/MainContainers'
+import SplashScreen from './navigation/screen/SplashScreen'
+
+const App = () => {
+  return (
+    <MainContainers />
+  )
+}
+
+export default App
